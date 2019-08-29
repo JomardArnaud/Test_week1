de7552ebbde7057eb3e79a49f2834afae020a329
JomardArnaud <arnaud.jomard@outlook.fr>
2019-08-29 14:18:17 +0200

    second day | good advance on connection with mySql server

--- connection.js
+++ connection.js
@@ -1,45 +1,60 @@
-const ctrl = module.exports;
 var mysql = require('mysql');
+const ctrl = module.exports;
 
 var con = mysql.createConnection({
   host: "localhost",
   user: "root",
-  password: "oppai8", //je suis pas sur de vouloir push ça xD
-  database: "MySQL80"
+  password: "", //je suis pas sur de vouloir push ça xD
+  database: "user_scheama",
+  port: "3308"
 });
 
-con.connect(function(err) {
-    if (err) throw err;
-    con.query("SELECT * FROM users", function (err, result, fields) {
-      if (err) throw err;
-      console.log(result);
-    });
-  });
-
 ctrl.login = function(request) {
-    con.connect();
-    compareLoginStuff(request.login, request.password);
-}
-
-function compareLoginStuff(login, password) {
-    loginStuffUser= getLoginStuff();
-    for(let i=0; i < loginStuffUser.User.length;i++) {
-        if (login == loginStuffUser.User[i].login) {
-            if (password == loginStuffUser.User[i].password) {
-                console.log("connection sucess");
-                return (0);
+    con.query("SELECT * FROM users", function (err, result, fields) {
+        if (err) throw err;
+        for(let i=0; i < result.length;i++) {
+            if (request.Login == result[i].Login) {
+                if (request.Password == result[i].Password) {
+                    console.log("connection sucess");
+                    return (0);
+                }
             }
         }
-    }
-    console.log("login or password might be wrong");
-    return (-1);
+        console.log("login or password might be wrong");
+        return (-1);
+    });
 }
 
-function getLoginStuff() {
-    //will be request SQL
-    var jsonLoginTmp = JSON.parse('{ "User" : [' +
-            '{ "login":"root1" , "password":"root1"},' +
-            '{ "login":"root2" , "password":"root2"},' +
-            '{ "login":"root3" , "password":"root3"} ]}');
-    return (jsonLoginTmp);
+ctrl.addUser = function(request) {
+    if (isLoginValid(request.Login)) {
+        if (isPasswordValid(request.Password)) {
+            con.query("INSERT INTO users (Login, Password) VALUE('" + request.Login + "', '" + request.Password + "');", function (err, result, fields) {
+                if (err) throw err;
+            });
+        } else {
+            console.log("Password isn't valid"); //ce message sera dans isPasswordValid pour la traçabilité de l'erreur    
+        }
+    } else {
+        console.log("Login isn't valid"); //ce message sera dans isLoginValid pour la traçabilité de l'erreur
+    }
+}
+
+ctrl.changePassword = function(request) {
+    if (isPasswordValid(request.Password)) {
+        //Comme ça fait pas d'erreur quand il ne trouve pas de Login il faut l'handle à la main
+        con.query("UPDATE users SET Password='" + request.Password + "' WHERE Login='" + request.Login + "';", function (err, result, fields) {
+            if (err) throw err;
+        });
+    } else {
+        console.log("Password isn't valid"); //ce message sera dans isPasswordValid pour la traçabilité de l'erreur
+    }
+}
+
+function isLoginValid(login) {
+    // LIST CHECK WHO ARE NEEDED
+    //check if the login is use
+    return true;
+}
+function isPasswordValid(password) {
+    return true;
 }
--- server.js
+++ server.js
@@ -6,6 +6,14 @@ app.get('/login', function (req, res) {
   connection.login(req.query)
 })
 
+app.get ('/addUser', function(req, res) {
+  connection.addUser(req.query)
+})
+
+app.get('/changePassword', function(req, res) {
+  connection.changePassword(req.query)
+})
+
 app.listen(3000, function () {
   console.log('Example app listening on port 3000!')
 })
